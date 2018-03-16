e1551027e0720ca821ff099d3d3feae4ff54b17b
mohit <mohitbs1@gmail.com>
2018-03-16 05:53:44 -0700

    lpthw ex12 updates

--- python/examples/lpthw/ex12_prompting_people.py
+++ python/examples/lpthw/ex12_prompting_people.py
@@ -3,3 +3,11 @@ height = raw_input("How tall are you? ")
 weight = raw_input("How much do you weight? ")
 
 print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+
+# From the Study Drills
+# pydoc is a useful utility to get help about commands
+# python - m pydoc raw_input
+# python - m pydoc open
+# python - m pydoc file
+# python - m pydoc os
+# python - m pydoc sys
